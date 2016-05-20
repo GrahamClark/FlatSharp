24704b72a840c03ef400a8d9cb4c42e0eadb252c
Graham Clark <grahamclarkuk@gmail.com>
2016-05-20 21:25:12 +0100

    added basic FetchBits method

--- FlatSharp/Program.cs
+++ FlatSharp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FlatSharp
 {
@@ -10,6 +6,27 @@ namespace FlatSharp
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var word = 0xBEEF;
+                Console.WriteLine(((word >> 12) & (~(-1 << 4))).ToString("X"));
+                Console.WriteLine(FetchBits(word, 15, 4).ToString("X"));
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        private static int FetchBits(int word, int high, int length)
+        {
+            var mask = ~(-1 << length);
+            return word >> (high - length + 1) & mask;
         }
     }
 }
